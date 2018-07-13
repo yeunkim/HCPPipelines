eb0fd10bbf1672c9be9f151e1123c8f011b69aab
Yeun Kim <yeunkim@gmail.com>
2018-07-13 09:23:35 -0700

    r package paths updated

--- rpackages.sh
+++ rpackages.sh
@@ -4,7 +4,7 @@
 wget https://cran.r-project.org/src/contrib/Archive/mvtnorm/mvtnorm_1.0-6.tar.gz
 wget https://cran.r-project.org/src/contrib/modeltools_0.2-21.tar.gz
 wget https://cran.r-project.org/src/contrib/strucchange_1.5-1.tar.gz
-wget https://cran.r-project.org/src/contrib/TH.data_1.0-8.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/TH.data/TH.data_1.0-8.tar.gz
 wget https://cran.r-project.org/src/contrib/Archive/multcomp/multcomp_1.4-6.tar.gz
 wget https://cran.r-project.org/src/contrib/Archive/coin/coin_1.2-1.tar.gz
 wget https://cran.r-project.org/src/contrib/lattice_0.20-35.tar.gz
@@ -13,12 +13,12 @@ wget https://cran.r-project.org/src/contrib/sandwich_2.4-0.tar.gz
 wget https://cran.r-project.org/src/contrib/Archive/party/party_1.0-25.tar.gz
 
 wget https://cran.r-project.org/src/contrib/e1071_1.6-8.tar.gz
-wget https://cran.r-project.org/src/contrib/kernlab_0.9-25.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/kernlab/kernlab_0.9-25.tar.gz
 wget https://cran.r-project.org/src/contrib/class_7.3-14.tar.gz
 
 wget https://cran.r-project.org/src/contrib/gplots_3.0.1.tar.gz
 wget https://cran.r-project.org/src/contrib/bitops_1.0-6.tar.gz
-wget https://cran.r-project.org/src/contrib/gtools_3.5.0.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/gtools/gtools_3.5.0.tar.gz
 wget https://cran.r-project.org/src/contrib/gdata_2.18.0.tar.gz
 wget https://cran.r-project.org/src/contrib/caTools_1.17.1.tar.gz
 wget https://rocr.bioinf.mpi-sb.mpg.de/ROCR_1.0-5.tar.gz
