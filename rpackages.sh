f4de58d515f6cf69026649ab232625b92a85775c
Yeun Kim <yeunkim@gmail.com>
2018-07-13 08:46:04 -0700

    switched order of AP and PA and fixed bug in template res

--- rpackages.sh
+++ rpackages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # pull r package tar balls
-wget https://cran.r-project.org/src/contrib/mvtnorm_1.0-6.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/mvtnorm/mvtnorm_1.0-6.tar.gz
 wget https://cran.r-project.org/src/contrib/modeltools_0.2-21.tar.gz
 wget https://cran.r-project.org/src/contrib/strucchange_1.5-1.tar.gz
 wget https://cran.r-project.org/src/contrib/TH.data_1.0-8.tar.gz
-wget https://cran.r-project.org/src/contrib/multcomp_1.4-6.tar.gz
-wget https://cran.r-project.org/src/contrib/coin_1.2-1.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/multcomp/multcomp_1.4-6.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/coin/coin_1.2-1.tar.gz
 wget https://cran.r-project.org/src/contrib/lattice_0.20-35.tar.gz
-wget https://cran.r-project.org/src/contrib/zoo_1.8-0.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/zoo/zoo_1.8-0.tar.gz
 wget https://cran.r-project.org/src/contrib/sandwich_2.4-0.tar.gz
 wget https://cran.r-project.org/src/contrib/Archive/party/party_1.0-25.tar.gz
 
@@ -23,8 +23,8 @@ wget https://cran.r-project.org/src/contrib/gdata_2.18.0.tar.gz
 wget https://cran.r-project.org/src/contrib/caTools_1.17.1.tar.gz
 wget https://rocr.bioinf.mpi-sb.mpg.de/ROCR_1.0-5.tar.gz
 
-wget https://cran.r-project.org/src/contrib/randomForest_4.6-12.tar.gz
-wget https://cran.r-project.org/src/contrib/MASS_7.3-47.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/randomForest/randomForest_4.6-12.tar.gz
+wget https://cran.r-project.org/src/contrib/Archive/MASS/MASS_7.3-47.tar.gz
 
 
 R CMD INSTALL kernlab_0.9-25.tar.gz
